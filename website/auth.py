17f8370c44b9d8a4a12bae166f615c98ca1a045f
robinb2009 <robinb2009@gmail.com>
2021-03-19 06:59:20 +0530

    Added Dashboard and extra pages, changed minimal connectivity of pages

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from pprint import pprint
+import random
 
 auth = Blueprint('auth', __name__)
 
@@ -10,7 +11,9 @@ def login():
         content = request.form
         #name of the form elements
         pprint(content.to_dict(flat=False))
-    return render_template("login.html")
+        return render_template("welcome.html")
+    else:
+        return render_template("login.html")
 
 @auth.route('/getData', methods=['GET', 'POST'])
 def getData():
@@ -32,6 +35,38 @@ def signup():
     if request.method == 'POST':
         content = request.form
         pprint(content.to_dict(flat=False))
-    return render_template("signup.html")
+        flash('Account Created!', category='success')
+        return login()
+    else:
+        return render_template("signup.html")
 
 
+@auth.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
+@auth.route('/metrics')
+def metrics():
+    return render_template("metrics.html")
+
+@auth.route('/dashboard')
+def dashboard():
+    
+    dataset = dict()
+    vals = ['hb', 'bp', 'sp', 'calories']
+
+    for measure in vals:
+        dataset[measure] = []
+        x = []
+        y = []
+        for i in range(10):
+            tx = random.randint(0,100)/10
+            ty = random.randint(900,1000)/10
+            x.append(tx)
+            y.append(ty)
+        x.sort()
+
+        for i in range(10):
+            dataset[measure].append({'x':x[i], 'y':y[i] })
+
+    return render_template("dashboard.html", data=dataset)
